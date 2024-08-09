59b799170b8d15cc05eaa4f8b4bf940ec2d5c539
rohit <rohit@kali.kali>
2024-08-09 13:47:59 +0530

    nothin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"}>
-      <Button variant={"unstyled"} color={"white"}>
+    <HStack p={"4"} shadow={"base"} bgColor={"#EDEADE"} style={{boxShadow:"box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;"}}>
+      <Button variant={"unstyled"} color={"blackAlpha.900"}>
         <Link to="/">Home</Link>
       </Button>
-      <Button variant={"unstyled"} color={"white"}>
+      <Button variant={"unstyled"} color={"blackAlpha.900"}>
         <Link to="/exchanges">Exchanges</Link>
       </Button>
-      <Button variant={"unstyled"} color={"white"}>
+      <Button variant={"unstyled"} color={"blackAlpha.900"}>
         <Link to="/coins">Coins</Link>
       </Button>
     </HStack>
